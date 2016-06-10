71abf3df85e09be6b705bdb708f557d44b380bea
Alexander Bronner <acbronn@gmail.com>
2016-06-10 15:35:17 -0400

    Those aren't squares. They're rectangles, silly!

--- js/class_scene_0.js
+++ js/class_scene_0.js
@@ -3,11 +3,11 @@ var Scene_0 = function()
 {
 	Scene.call(this);
     
-    this.gameObjects.push(new BasicSquare(new Vect(100, 100, 0)));
-    this.gameObjects.push(new BasicSquare(new Vect(100, 200, 0)));
-    this.gameObjects.push(new BasicSquare(new Vect(200, 100, 0)));
-    this.gameObjects.push(new BasicSquare(new Vect(200, 200, 0)));
-    this.gameObjects.push(new AnimatedSquare(new Vect(320, 240, 0)));
+    this.gameObjects.push(new BasicRect(new Vect(100, 100, 0)));
+    this.gameObjects.push(new BasicRect(new Vect(100, 200, 0)));
+    this.gameObjects.push(new BasicRect(new Vect(200, 100, 0)));
+    this.gameObjects.push(new BasicRect(new Vect(200, 200, 0)));
+    this.gameObjects.push(new AnimatedRect(new Vect(320, 240, 0)));
 }
 
 Scene_0.prototype = Object.create(Scene.prototype);
